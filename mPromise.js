c0c2ef6cd165fcf5bdddeaf0543960f8489ae60f
Swordword <massivejohn123@gmail.com>
2020-11-20 00:48:48 +0800

    update

--- mPromise.js
+++ mPromise.js
@@ -1,20 +1,4 @@
-let mClass = (function () {
-  let mClass = function (name) {
-    if (typeof new.target === undefined) {
-      throw new Error('Constructor must be called by new')
-    }
-    this.name = name
-  }
-  Object.defineProperty(mClass.prototype, 'sayName', {
-    value: function () {
-      if (typeof new.target !== undefined) {
-        throw new Error('Method cannot be called by new')
-      }
-      console.log(this.name)
-    },
-    enumerable: true,
-    configurable: true,
-    writable: true,
-  })
-  return mClass
-})()
+/**
+ * 
+ */
+let mPromise = 
